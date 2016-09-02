63251992c55e7e24a338af8331ecd8bd99e3bf04
Charmel Sippio <charmelsippio@gmail.com>
2016-09-02 12:48:13 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// $(document).ready(function(){
+  $('.parallax-window').parallax({imageSrc: 'images/bg_about.png'}); 
+  $('.parallax-window').parallax({imageSrc: 'images/bg_projects.png'});
+  $('.parallax-window').parallax({imageSrc: 'images/bg_experience.png'});
+  $('.parallax-window').parallax({imageSrc: 'images/bg_contact.png'});
+// })
